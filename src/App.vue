5e4569e1f625673ffb955d892c4ae4d5355a7612
greg <gregorybwest@gmail.com>
2022-04-01 14:54:22 -0700

    adds flexible bootstrap nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,51 @@
+<script>
+export default {
+  data: function () {
+    return {
+      isLoggedIn: !!localStorage.jwt,
+      flashMessage: null,
+    };
+  },
+  watch: {
+    $route: function () {
+      this.isLoggedIn = !!localStorage.jwt;
+      this.flashMessage = localStorage.flashMessage;
+      localStorage.removeItem("flashMessage");
+    },
+  },
+  methods: {},
+};
+</script>
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Blogs</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-link active" v-if="!isLoggedIn" to="/signup">Signup</router-link>
+          <router-link class="nav-link" v-if="!isLoggedIn" to="/login">Login</router-link>
+          <router-link class="nav-link" v-if="isLoggedIn" to="/logout">Logout</router-link>
+          <router-link class="nav-link" v-if="isLoggedIn" to="/posts/new">Create Post</router-link>
+          <router-link class="nav-link" to="/posts">Posts</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div v-if="flashMessage">
+    {{ flashMessage }}
+    <button v-on:click="flashMessage = ''">Dismiss</button>
   </div>
   <router-view />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
